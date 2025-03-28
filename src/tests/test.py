149307f9d6fc2a24bdb90482a5cc9c7a3e18b518
viktor-trump <viktorkozyr2@gmail.com>
2025-03-28 18:02:07 +0300

    refactoring

--- src/tests/test.py
+++ src/tests/test.py
@@ -17,13 +17,13 @@ def test_withdraw_balance():
 def test_withdraw_more_balance():
     account = BankAccount("Антон", 100)
     account.withdraw(150)
-    assert account.get_balance() == 100.0  # Balance should remain the same
+    assert account.get_balance() == 100.0
 
 def test_negative_deposit():
     account = BankAccount("Антон", 100)
     account.deposit(-50)
-    assert account.get_balance() == 100.0  # Balance should remain the same
+    assert account.get_balance() == 100.0
 
 def test_balance_is_positive():
     account = BankAccount("Антон", 100)
-    assert account.get_balance() > 0  # Check that balance is greater than 0
+    assert account.get_balance() > 0
